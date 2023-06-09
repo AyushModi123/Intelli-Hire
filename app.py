64226805675b908adec60c87427751da1efac446
AyushModi123 <ayushonweb785@gmail.com>
2023-06-09 14:12:43 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request, session, redirect, url_for, redirect
 from pymongo import MongoClient
-import io
+import asyncio
+from scraper_cp import scrape, data_cleaning
+from gen_grade import data_extraction
+import threading
+import multiprocessing
+def scraping(links):
+    print('scraping')
+    while links['links'] == None:
+        pass
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    codingData = loop.run_until_complete(scrape())
+    loop.close()
+    cleaned_data = data_cleaning().clean_data(codingData)
+    print(cleaned_data)
 class app:
     def __init__(self) -> None:
         self.resume = None
@@ -20,12 +34,45 @@ class app:
             'Frontend',
             # Add more domain options as needed
         ]
+        
+        def resume_print():
+            print('resume_print')
+            while self.resume == None:
+                pass
+            print(self.resume.filename)
+            de = data_extraction(self.resume.filename)
+            print(de.context)
+            # jd = '''Selected intern's day-to-day responsibilities include:
 
+            # 1. Writing algorithms in Python (at an advanced level) for predictive healthcare
+            # 2. Working on pre-processing data
+            # 3. Testing various hypotheses using statistical measures
+            # 4. Conducting scientific research
+            # 5. Developing mathematical algorithms
+
+            # Additional Information:
+
+            # We are looking for exceptional interns with advanced programming skills and a good understanding of data science. In addition to the minimum assured stipend, the interns may also receive additional incentives of up to Rs. 2000 on the basis of their performance.'''
+            # de.jd_comparator(jd)
+            # global result
+            # result = de.output_grade()
+            # print(result)
+
+        manager = multiprocessing.Manager()
+        links = manager.dict()
+        links['links'] = None
+        thread1 = threading.Thread(target=resume_print)
+        thread1.start()
+        p = multiprocessing.Process(target=scraping, args=(links,))
+        p.start()
+        
+        
         @app.route('/', methods=['GET', 'POST'])
         def upload():
             if request.method == 'POST':
                 self.resume = request.files['resume']
                 self.resume.save(self.resume.filename)
+                links['links'] = 'ad kjn'
                 return redirect('/quiz')
             if request.method == 'GET':
                 return render_template('upload.html')
@@ -62,16 +109,24 @@ class app:
                 return redirect(url_for('quiz'))
 
             if session['current_question'] >= len(questions):
-                score = session['score']
-                session.clear()
-                return render_template('score.html', score=score, question_length=len(questions))
+                # score = session['score']
+                # session.clear()
+                # return render_template('score.html', score=score, question_length=len(questions))
+                return redirect('/result')
 
             current_question = questions[session['current_question']]
             return render_template('quiz.html', question=current_question['question'], choices=current_question['choices'],
                                 current_question=session['current_question'])
+        @app.route('/result', methods=['GET', 'POST'])
+        def result():
+            thread1.join()
+            p.join()    
+            score = session['score']
+            session.clear()
+            return render_template('score.html', score=score, question_length=len(questions))
 
-        app.run(debug=True)
-        return session['score']
+        app.run(debug=False)
+        # return session['score']
 
 
 
