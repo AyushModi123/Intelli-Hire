69f4afad5bcb8ee06c04f9462d57b63e48600524
AyushModi123 <ayushonweb785@gmail.com>
2023-06-09 19:41:20 +0530

    update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def scraping(links):
 class app:
     def __init__(self) -> None:
         self.resume = None
-
+        self.result = {}
     def run_flask_app(self):
         app = Flask(__name__)
         app.secret_key = 'your_secret_key'
@@ -37,41 +37,37 @@ class app:
         
         def resume_print():
             print('resume_print')
-            while self.resume == None:
-                pass
             print(self.resume.filename)
             de = data_extraction(self.resume.filename)
             print(de.context)
-            # jd = '''Selected intern's day-to-day responsibilities include:
+            jd = '''Selected intern's day-to-day responsibilities include:
 
-            # 1. Writing algorithms in Python (at an advanced level) for predictive healthcare
-            # 2. Working on pre-processing data
-            # 3. Testing various hypotheses using statistical measures
-            # 4. Conducting scientific research
-            # 5. Developing mathematical algorithms
+            1. Writing algorithms in Python (at an advanced level) for predictive healthcare
+            2. Working on pre-processing data
+            3. Testing various hypotheses using statistical measures
+            4. Conducting scientific research
+            5. Developing mathematical algorithms
 
-            # Additional Information:
+            Additional Information:
 
-            # We are looking for exceptional interns with advanced programming skills and a good understanding of data science. In addition to the minimum assured stipend, the interns may also receive additional incentives of up to Rs. 2000 on the basis of their performance.'''
-            # de.jd_comparator(jd)
-            # global result
-            # result = de.output_grade()
-            # print(result)
+            We are looking for exceptional interns with advanced programming skills and a good understanding of data science. In addition to the minimum assured stipend, the interns may also receive additional incentives of up to Rs. 2000 on the basis of their performance.'''
+            de.jd_comparator(jd)
+            self.result = de.output_grade()
+            print(self.result)
 
         manager = multiprocessing.Manager()
         links = manager.dict()
         links['links'] = None
         thread1 = threading.Thread(target=resume_print)
-        thread1.start()
         p = multiprocessing.Process(target=scraping, args=(links,))
-        p.start()
-        
         
         @app.route('/', methods=['GET', 'POST'])
         def upload():
             if request.method == 'POST':
                 self.resume = request.files['resume']
                 self.resume.save(self.resume.filename)
+                thread1.start()
+                p.start()
                 links['links'] = 'ad kjn'
                 return redirect('/quiz')
             if request.method == 'GET':
@@ -109,28 +105,26 @@ class app:
                 return redirect(url_for('quiz'))
 
             if session['current_question'] >= len(questions):
-                # score = session['score']
-                # session.clear()
-                # return render_template('score.html', score=score, question_length=len(questions))
                 return redirect('/result')
 
             current_question = questions[session['current_question']]
             return render_template('quiz.html', question=current_question['question'], choices=current_question['choices'],
                                 current_question=session['current_question'])
-        @app.route('/result', methods=['GET', 'POST'])
+        @app.route('/result', methods=['GET'])
         def result():
             thread1.join()
             p.join()    
             score = session['score']
+            self.result['Test Score'] = score
             session.clear()
-            return render_template('score.html', score=score, question_length=len(questions))
+            if request.method == 'GET':
+                return render_template('score.html', score=score, question_length=len(questions))
 
+            
         app.run(debug=False)
-        # return session['score']
 
 
 
 if __name__ == '__main__':
     ap = app()
     ap.run_flask_app()
-
